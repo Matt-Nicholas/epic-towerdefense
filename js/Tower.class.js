54db8d8f7bca09c7cad2edc8ba8a06f063575641
Garrett Boggs and Jason Downing <garrett+jason@>
2016-11-07 15:06:47 -0800

    add upgrades to towers

--- js/Tower.class.js
+++ js/Tower.class.js
@@ -1,4 +1,5 @@
 var type;
+var level;
 
 var Tower = function(worldX, worldY, tileX, tileY, tile) {
     var index = String(eval(tileX + "" + tileY));
@@ -17,12 +18,14 @@ var Tower = function(worldX, worldY, tileX, tileY, tile) {
       if(towerSelected === 'bitchin') {
         this.tower.fireTime = 500;
         this.tower.radius = 100;
+        this.tower.level = 1;
         this.tower.type = "bitchin";
         towers.add(this.tower);
       } else {
         this.tower = game.add.sprite(worldX, worldY, tile);
         this.tower.fireTime = 1000;
         this.tower.radius = 200;
+        this.tower.level = 1;
         this.tower.type = "meh";
         towers.add(this.tower);
       }
